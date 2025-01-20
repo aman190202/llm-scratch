18e4dc76873a319dbdf748444d38320548fd7770
Aman Agarwal <agarwalaman190202@gmail.com>
2025-01-19 21:02:59 -0500

    Self attention class added

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 import tiktoken
+import torch.nn as nn
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self,d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+    
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self,d_in,d_out,qkv_bias = False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_keys = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_values = nn.Linear(d_in, d_out, bias = qkv_bias)
+
+    def forward(self,x):
+        keys = self.W_keys(x)
+        queries = self.W_query(x)
+        values = self.W_values(x)
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+
+
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
---
+++ selfAttention.py
@@ -0,0 +1,47 @@
+import torch
+
+
+
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+# Dot product between each vector tells us how similar they are
+attention_scores = inputs @ inputs.T
+attention_weights = torch.softmax(attention_scores, dim=-1)
+all_context_vecs = attention_weights @ inputs # defined by z
+
+
+# scaled dot product attention -- introduction of weight matrices that are trainable
+# w_q, w_k, w_v - query, key and value vector
+
+
+x_2 = inputs[1]
+d_in = inputs.shape[1] # input embedding size
+d_out = 2 # usually the same with the input embedding side
+
+
+# W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+# W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+# W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+# query_2 = x_2 @ W_query 
+# key_2 = x_2 @ W_key 
+# value_2 = x_2 @ W_value
+
+# keys = inputs @ W_key
+# values = inputs @ W_value
+
+from main import SelfAttention_v1
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+
+# the reason for normalization is to improve the training performace by avoiding small gradients
