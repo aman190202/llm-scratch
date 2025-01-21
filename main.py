d6e781680d522d09e5786776f4611ef896dc4f7c
Aman Agarwal <agarwalaman190202@gmail.com>
2025-01-21 00:14:37 -0500

    Causal Attention added

--- main.py
+++ main.py
@@ -41,6 +41,37 @@ class SelfAttention_v2(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
 
+class CausalAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+                dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)            #1
+        self.register_buffer(
+           'mask',
+           torch.triu(torch.ones(context_length, context_length),
+           diagonal=1)
+        )             #2
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape                   #3
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)   
+        attn_scores.masked_fill_(                    #4
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf) 
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = attn_weights @ values
+        return context_vec
 
 
 class GPTDatasetV1(Dataset):
--- selfAttention.py
+++ selfAttention.py
@@ -38,10 +38,17 @@ d_out = 2 # usually the same with the input embedding side
 # keys = inputs @ W_key
 # values = inputs @ W_value
 
-from main import SelfAttention_v1
+from main import SelfAttention_v1, SelfAttention_v2
 torch.manual_seed(123)
 sa_v1 = SelfAttention_v1(d_in, d_out)
 print(sa_v1(inputs))
 
 
 # the reason for normalization is to improve the training performace by avoiding small gradients
+
+
+# Causal Attention (masked attention) - hiding the future
+
+sa_v2 = SelfAttention_v2(d_in, d_out)
+queries = sa_v2.W_keys(inputs)
+keys = sav
